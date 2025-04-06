c8864d74ba13dc1eee700f6a97aa632d452bec55
asd2d <872122@mail.ru>
2025-04-06 23:22:11 +0300

    add api resource routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskStatusController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResources([
+    'tasks' => TaskController::class,
+    'task_statuses' => TaskStatusController::class,
+]);
