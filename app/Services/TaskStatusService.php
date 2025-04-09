0eec60188d17a1c0a04961e1bc324f58ec624a1a
asd2d <872122@mail.ru>
2025-04-09 23:16:06 +0300

    create TaskStatusResource and update TaskStatusService

--- app/Services/TaskStatusService.php
+++ app/Services/TaskStatusService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Http\Resources\TaskStatusResource;
 use App\Interfaces\TaskStatusServiceInterface;
 use App\Models\TaskStatus;
 
@@ -9,23 +10,23 @@ class TaskStatusService implements TaskStatusServiceInterface
 {
     public function index()
     {
-        return TaskStatus::all();
+        return TaskStatusResource::collection(TaskStatus::all());
     }
 
     public function store(array $data)
     {
-        return TaskStatus::create($data);
+        return new TaskStatusResource(TaskStatus::create($data));
     }
 
     public function show(TaskStatus $taskStatus)
     {
-        return $taskStatus;
+        return new TaskStatusResource($taskStatus);
     }
 
     public function update(TaskStatus $taskStatus, array $data)
     {
         $taskStatus->update($data);
-        return $taskStatus;
+        return new TaskStatusResource($taskStatus);
     }
 
     public function destroy(TaskStatus $taskStatus)
